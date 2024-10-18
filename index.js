ddbf25cc8baa9e1b5d819fa3c00f2e78bf7425a5
Connie <c.amer@icloud.com>
2024-10-18 19:11:06 +0100

    slideshow carousel bug fix

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ function outFunc() {
 }
 
 let slideIndex = 1;
-showSlides(slideIndex);
+if (document.getElementsByClassName("mobile").style.display === "none") {
+  showSlides(slideIndex);
+}
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -26,6 +28,34 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  slides[slideIndex-1].style.justifyContent = "center";
+  slides[slideIndex-1].style.alignItems = "center";
+}
+
+let mobileSlideIndex = 1;
+if (document.getElementsByClassName("mobile").style.display === "flex") {
+  showMobileSlides(mobileSlideIndex);
+}
+
+function plusMobileSlides(n) {
+  if (document.getElementsByClassName("mobile").style.display === "flex") {
+    showMobileSlides(mobileSlideIndex += n);
+  }
+}
+
+function currentMobileSlide(n) {
+  showMobileSlides(mobileSlideIndex = n);
+}
+
+function showMobileSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slideMobile");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
