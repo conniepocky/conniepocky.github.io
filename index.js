16947f58a207f907115d84517716cf13d25882cf
Connie <c.amer@icloud.com>
2024-10-18 17:20:21 +0100

    slideshow portfolio section

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 function copyToClipboard() {
-    var copyText = "cpa.dev.contact@gmail.com";
-    navigator.clipboard.writeText(copyText);
-    
-    var popup = document.getElementById("myPopup");
-    
-    popup.innerHTML = "Copied to clipboard"
+  var copyText = "cpa.dev.contact@gmail.com";
+  navigator.clipboard.writeText(copyText);
+  
+  var popup = document.getElementById("myPopup");
+  
+  popup.innerHTML = "Copied to clipboard"
 }
 
 function outFunc() {
-    var popup = document.getElementById("myPopup");
-    popup.innerHTML = "Copy to clipboard";
+  var popup = document.getElementById("myPopup");
+  popup.innerHTML = "Copy to clipboard";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  slides[slideIndex-1].style.justifyContent = "center";
+  slides[slideIndex-1].style.alignItems = "center";
 }
