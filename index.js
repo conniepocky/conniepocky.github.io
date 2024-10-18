d91744a656f2ec350d494bffc68a932b8f64fee8
Connie <c.amer@icloud.com>
2024-10-18 19:49:57 +0100

    slideshow carousel mobile

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ function outFunc() {
 }
 
 let slideIndex = 1;
-if (document.getElementsByClassName("mobile").style.display === "none") {
-  showSlides(slideIndex);
-}
+showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -27,35 +25,13 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  let slides;
+
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    slides = document.getElementsByClassName("slideMobile");
+  } else {
+    slides = document.getElementsByClassName("slide");
   }
-  slides[slideIndex-1].style.display = "flex";  
-  slides[slideIndex-1].style.justifyContent = "center";
-  slides[slideIndex-1].style.alignItems = "center";
-}
-
-let mobileSlideIndex = 1;
-if (document.getElementsByClassName("mobile").style.display === "flex") {
-  showMobileSlides(mobileSlideIndex);
-}
-
-function plusMobileSlides(n) {
-  if (document.getElementsByClassName("mobile").style.display === "flex") {
-    showMobileSlides(mobileSlideIndex += n);
-  }
-}
-
-function currentMobileSlide(n) {
-  showMobileSlides(mobileSlideIndex = n);
-}
-
-function showMobileSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slideMobile");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
